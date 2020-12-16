02138d1989671937461404626078bb94c95cd261
fernando duenas <fernando39615@aim.com>
2020-12-16 15:45:24 -0500

    commit

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
+const xhr = new XMLHttpRequest();
 const button = document.querySelector('.button');
-const inputP = document.querySelector('.text');
-const sectionP = document.querySelector('#section-p');
-const url = 'https://dog.ceo/api/breeds/list/all';
+const img = document.querySelector('img');
+// const inputP = document.querySelector('.text');
+// const sectionP = document.querySelector('#section-p');
+const url = 'https://dog.ceo/api/breeds/image/random';
+
+xhr.addEventListener('loadend', () => {
+    const data = JSON.parse(xhr.responseText);
+    console.log(data);
+    button.innerText = ' Another Dog ! '
+    img.src = data.message
+});
 
 button.addEventListener('click', () => {
-    const xhr = new XMLHttpRequest();
-    newElem('https://dog.ceo/api/breeds/image/random');
-    xhr.addEventListener('loadend', () => {
-    const data = 
-    JSON.parse(xhr.responseText);
-        console.log(data);
-    });
-
+    
     xhr.open('GET', url)
     xhr.setRequestHeader('Accept', 'application/json');
     xhr.send();
 });
 
-const newElem = (input) => {
-    const newPara = document.createElement('img');
-    sectionP.appendChild(newPara);
-    newPara.innerText = input
-}
+// const newElem = (input) => {
+//     const newPara = document.createElement('img');
+//     sectionP.appendChild(newPara);
+//     newPara.innerText = input
+// }
+// newElem('https://dog.ceo/api/breeds/image/random');
 
